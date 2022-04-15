b6864ba46f73174130544fd71b508268717df6e4
CozyBrian <dragonbrian33@gmail.com>
2022-04-15 01:55:39 +0000

    misc: removed smoothscroll js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import smoothscroll from "smoothscroll-polyfill";
-
-window.__forceSmoothScrollPolyfill__ = true;
-smoothscroll.polyfill();
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
