068f651adb23eefd8d6332bcce7f2cb67fb3c264
CozyBrian <dragonbrian33@gmail.com>
2022-04-19 20:16:37 +0000

    fix: replace placeholder text

--- src/components/contactMe/contact-me.js
+++ src/components/contactMe/contact-me.js
@@ -14,28 +14,35 @@ const ContactMeView = () => {
               <i className="fa-solid fa-envelope" />
             </span>
             <p className="icon-label">EMAIL</p>
-            <p className="contact-info">Email@email.com</p>
+            <p className="contact-info">Kwabbenanewton02@gmail.com</p>
           </div>
           <div className="clg-3 cmd-6 csm cont">
             <span className="icon">
               <i className="fa-solid fa-mobile-button" />
             </span>
             <p className="icon-label">PHONE</p>
-            <p className="contact-info">+233 23 345 5325</p>
+            <p className="contact-info">+233 50 258 6042</p>
           </div>
           <div className="clg-3 cmd-6 csm cont">
             <span className="icon">
               <i className="fa-brands fa-github" />
             </span>
             <p className="icon-label">GITHUB</p>
-            <p className="contact-info">CozyWho</p>
+            <a href="https://github.com/CozyBrian" className="contact-info">
+              CozyBrian
+            </a>
           </div>
           <div className="clg-3 cmd-6 csm cont">
             <span className="icon">
               <i className="fa-brands fa-linkedin" />
             </span>
             <p className="icon-label">LINKEDIN</p>
-            <p className="contact-info">Kwabena Newton</p>
+            <a
+              href="https://www.linkedin.com/in/brian-newton-b6b8231b8/"
+              className="contact-info"
+            >
+              Kwabena Newton
+            </a>
           </div>
         </div>
       </div>
