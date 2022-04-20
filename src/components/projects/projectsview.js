ed03cd487ab6b2a5580a9449af3c96af81329a55
CozyBrian <dragonbrian33@gmail.com>
2022-04-20 00:22:11 +0000

    feat: pull projects from firebase

--- src/components/projects/projectsview.js
+++ src/components/projects/projectsview.js
@@ -1,22 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ApiContext } from "../../services/api.context";
 import ProjectCard from "./project-card";
 import "./styles.css";
 
 const ProjectView = () => {
-  const image = require("./IMG_0471.PNG");
-
-  const item = {
-    title: "Screenshot",
-    disc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    image: require("./IMG_0471.PNG"),
-  };
+  const { isLoaded, projects } = useContext(ApiContext);
 
   return (
     <div className="row proj-box-con" id="projects">
       <h1 className="heading">PROJECTS</h1>
-      <ProjectCard image={item.image} title={item.title} disc={item.disc} />
-      <ProjectCard image={item.image} title={item.title} disc={item.disc} />
-      <ProjectCard image={item.image} title={item.title} disc={item.disc} />
+      {projects.map((item) => (
+        <ProjectCard image={item.image} title={item.title} disc={item.disc} />
+      ))}
     </div>
   );
 };
