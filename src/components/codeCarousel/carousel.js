2544a60524b3a0bddbd3c21998fabb37df9e10e9
CozyBrian <dragonbrian33@gmail.com>
2022-04-20 01:31:00 +0000

    feat: profile image

--- src/components/codeCarousel/carousel.js
+++ src/components/codeCarousel/carousel.js
@@ -3,6 +3,9 @@ import CodeWindow from "./codeWindow";
 import "./styles.css";
 
 const Carousel = () => {
+  const profile =
+    "https://firebasestorage.googleapis.com/v0/b/my-portfolio-35b84.appspot.com/o/profilepic.jpg?alt=media&token=bf734f1e-96de-4610-ab49-7d64a15ed653";
+
   return (
     <div className="carousel row" id="home">
       <div className="col-lg-8 ">
@@ -10,7 +13,11 @@ const Carousel = () => {
       </div>
       <div className="image-view-side col-md">
         <div className="image-view-box">
-          <div className="imagee"></div>
+          <img
+            src={profile}
+            className="profile-image"
+            alt="a handsome face"
+          ></img>
         </div>
       </div>
     </div>
