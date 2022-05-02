3bad3a21666cf3bd311e878030e8c93fabfe089a
CozyBrian <dragonbrian33@gmail.com>
2022-05-02 23:52:56 +0000

    feat: tab view for projects

--- src/components/projects/projectsview.js
+++ src/components/projects/projectsview.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 import { ApiContext } from "../../services/api.context";
 import ProjectCard from "./project-card";
 import "./styles.css";
@@ -6,21 +8,46 @@ import "./styles.css";
 const ProjectView = () => {
   const { projects, isLoaded } = useContext(ApiContext);
 
+  const item = {
+    disc: "MealsToGo is a restauant and food look-up app.You can find restaurants and check their menu and opening hours. Uses and api to fetch restaurants.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-35b84.appspot.com/o/projects-screenshots%2Fmealstogo1.PNG?alt=media&token=ce25fe08-06da-40bb-906a-f1f2e9534dbe",
+    title: "MealsToGo",
+    link: "https://github.com/CozyBrian/MealsToGo",
+  };
+
   return (
     <div className="row proj-box-con" id="projects">
       <h1 className="heading">PROJECTS</h1>
-      {isLoaded ? (
-        projects.map((item) => (
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home">
+          {isLoaded ? (
+            projects.map((item, i) => (
+              <ProjectCard
+                key={i}
+                image={item.image}
+                title={item.title}
+                disc={item.disc}
+                link={item.link}
+              />
+            ))
+          ) : (
+            <h3>Error loading Projects.</h3>
+          )}
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
           <ProjectCard
             image={item.image}
             title={item.title}
             disc={item.disc}
             link={item.link}
           />
-        ))
-      ) : (
-        <h3>Error loading Projects.</h3>
-      )}
+        </Tab>
+      </Tabs>
     </div>
   );
 };
